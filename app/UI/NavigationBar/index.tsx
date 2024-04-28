d046fae10947fab3ca58c1705f303880483f1b93
Yash <yash@Yashs-MacBook-Pro.local>
2024-04-28 22:06:56 +0530

    feat: added  navigation bar

---
+++ app/UI/NavigationBar/index.tsx
@@ -0,0 +1,19 @@
+import { ORANGE, VOILET } from '@/app/colors'
+import React from 'react'
+
+const NavigationBar = () => {
+  return (
+    <div style={{
+      padding: 16,
+      background: VOILET[100]
+    }}>
+      <h3 style={{
+        fontSize: "1.5rem"
+      }}>Yash <span style={{
+        color: ORANGE[100]
+      }}>Rawat</span></h3>
+    </div>
+  )
+}
+
+export default NavigationBar
---
+++ app/colors.ts
@@ -0,0 +1,10 @@
+export const VOILET={
+    100: "#3D0750",
+    200: "#451555",
+}
+export const ORANGE={
+    100: "#FF9C1A",
+}
+export const BLUE={
+    100: "#070024",
+}

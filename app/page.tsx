f4b606dcdaf665a48089dbc133e71ed0efdee520
Yash <yash@Yashs-MacBook-Pro.local>
2024-05-04 19:46:35 +0530

    feat: removed header for good

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,9 @@ const HomePage = () => {
     <div style={{
       background: BLUE[100],
     }}>
-    <NavigationBar/>
 
     <div style={{
-      display: "flex",
-      alignItems:"center",
-      justifyContent:"center",
-      minHeight:"100vh"
+      display: "flex",  alignItems:"center", justifyContent:"center", minHeight:"100vh"
     }}>
       <ProfileCardContainer>
         <ProfileContainer/>

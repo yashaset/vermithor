1658a6f21e224343d39110d8e7bd4274782a7bc6
Yash Rawat <39219898+yashaset@users.noreply.github.com>
2025-01-08 22:36:34 +0530

    chore: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { BLUE, ORANGE, VOILET } from "./colors";
+import { BLUE, ORANGE } from "./colors";
 import {
   InfoTileContainer,
   ProfileCardContainer,
